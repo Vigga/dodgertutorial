84c866639846bfe8999a26bca279113303ae446d
Vincent Quarles <vnq23@drexel.edu>
2015-08-22 09:50:36 -0400

    Initial Project Upload

---
+++ Dodger/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed = 8F;				//speed of Enemy
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, -speed * Time.deltaTime, 0);			//Movement of Enemy
+	}
+
+	//Checks when a Collider enters this Trigger
+	void OnTriggerEnter2D(Collider2D other){
+
+		//Checks if other gameobject has a Tag of Player
+		if(other.gameObject.tag == "Player"){
+			Time.timeScale = 0;			//Pauses gameplay
+		}
+	}
+}
